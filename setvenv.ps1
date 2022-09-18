c781e071adf45c42c328239743e454c61da8ffd0
pedroelir <pedro_r_b777@hotmail.com>
2022-09-18 16:21:42 +0200

    create a class for venv

--- setvenv.ps1
+++ setvenv.ps1
@@ -1,47 +1,59 @@
-function New-Venv {
-    param (
-        $PythonVersion = 3,
-        $EnvDir
-    )
-    Write-Host "Creating virtual environment in $($EnvDir)"
-    Invoke-Expression "py -$PythonVersion -m venv $EnvDir"
-}
-function Set-Venv {
-    param (
-        $EnvDir
-    )
-    Write-Host "Activating virtual environment"
-    Invoke-Expression $EnvDir\Scripts\Activate.ps1
-    Write-Host "Virtual environment activated"
+class Venv {
+    [string]$PythonVersion
+    [string]$EnvDir
+    [string]$EnvAbsPath
+    
+    Venv($PythonVersion = "3", $EnvDir = ".venv") {
+        $this.PythonVersion = $PythonVersion
+        $this.EnvDir = $EnvDir
+        $this.EnvAbsPath = $this.Create()
+        Write-Host "Virtual environment in $($this.EnvAbsPath)"
+    }
+
+    [string]Create() {
+        Write-Host "Creating virtual environment in $($this.EnvDir)"
+        $cmd = "py -$($this.PythonVersion) -m venv $($this.EnvDir)"
+        Invoke-Expression $cmd
+        $path = Resolve-Path $this.EnvDir
+        return $path
+    }
+
+    [void]Activate() {
+        Write-Host "Activating virtual environment in  $($this.EnvAbsPath)"
+        $cmd = "$($this.EnvAbsPath)\Scripts\Activate.ps1"
+        Invoke-Expression $cmd
+        Write-Host "Virtual environment activated"
+    }
+
+    [void]Deactivate() {
+        try {
+            Write-Host "Deactivating virtual environment"
+            Invoke-Expression deactivate
+            Write-Host "Virtual environment deactivated"
+        }
+        catch [System.Management.Automation.CommandNotFoundException] {
+            Write-Host "Virtual environment already deactivated"
+        }
+    }
+
+    [void]Remove() {
+        $this.Deactivate()
+        Write-Host "Removing virtual environment in $($this.EnvAbsPath)"
+        Remove-Item $this.EnvAbsPath -Recurse
+    }
 }
 
-function Reset-Venv {
-    try {
-        Write-Host "Deactivating virtual environment"
-        Invoke-Expression deactivate
-        Write-Host "Virtual environment deactivated"
-    }
-    catch [System.Management.Automation.CommandNotFoundException] {
-        Write-Host "Virtual environment already deactivated"
-    }
-}
-function Remove-Venv {
-    param (
-        $EnvDir
-    )
-    Write-Host "Removing virtual environment in $($EnvDir)"
-    Remove-Item $EnvDir -Recurse
-}
 
-$EnvDir = "venv"
-$PythonVersion = 3.8
+# }
+
+$EnvDir = ".venv"
+$PythonVersion = "3.8"
 $Script = ".\say_hello.ps1"
 
-New-Venv -PythonVersion $PythonVersion -EnvDir $EnvDir
-Set-Venv -EnvDir $EnvDir
-Start-Sleep(2)
+
+
+[Venv]$MyVenv = [Venv]::new($PythonVersion, $EnvDir)
+$MyVenv.Activate()
 Invoke-Expression $Script
 Start-Sleep(2)
-Reset-Venv -EnvDir $EnvDir
-Start-Sleep(2)
-Remove-Venv -EnvDir $EnvDir
+$MyVenv.Remove()
