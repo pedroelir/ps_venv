c8475b612233b2232fa5df28ade49e823b5f56e0
pedroelir <pedro_r_b777@hotmail.com>
2022-10-02 14:18:17 +0200

    add Use-Python function

--- setvenv.psm1
+++ setvenv.psm1
@@ -90,3 +90,54 @@ function GetPipExe {
     [Venv]$MyVenv = [Venv]::new($PythonVersion, $EnvDir)
     return $MyVenv.GetPipExe()
 }
+
+function Use-Python {
+    param (
+        [string]$default_python = "3.10"
+    )
+    <#
+        .SYNOPSIS
+        Check the use of the specified python Version
+
+        .DESCRIPTION
+        Checks if python is installed.
+        If it is installed it checks if the version mayor and minor is greater or equal to the default version provided.
+        If the Python version is not provided, message is return htat it is not install.
+        Further implementation can add Downloading and installing python
+
+        .PARAMETER default_python
+        Specify the Python version to use
+
+        .EXAMPLE
+        PS> Use-Python "3.11"
+        3.10.6150.1013
+        Python 3.10 not installed. How dare you???? Now installing against your will!!!!!!
+
+        .EXAMPLE
+        PS> Use-Python -default_python "3.10"
+        3.10.6150.1013
+        3.10.6150.1013 is a valid version greater or equal to 3.10
+
+    #>
+    
+    $expected_py_ver = $default_python.Split(".")
+    $expected_py_ver_mayor = [int]$expected_py_ver[0]
+    $expected_py_ver_minor = [int]$expected_py_ver[1]
+
+    $_pythonobj = Get-Command python -ErrorAction SilentlyContinue
+    # Write-Host $_pythonobj.Version
+    if ($_pythonobj -isnot [System.Management.Automation.CommandInfo]) {
+        Write-Host "NOOO"
+        Write-Host "Python $default_python not installed. How dare you???? Now installing against your will!!!!!!"
+    }
+    else {
+        Write-Host $_pythonobj.Version
+        if (($_pythonobj.Version.Major -ge $expected_py_ver_mayor) -and ($_pythonobj.Version.Minor -ge $expected_py_ver_minor)) {
+            Write-Host "$($_pythonobj.Version) is a valid version greater or equal to $default_python"
+        }
+        else {
+            Write-Host "Python $default_python not installed. How dare you???? Now installing against your will!!!!!!"
+        }
+    }
+
+}
